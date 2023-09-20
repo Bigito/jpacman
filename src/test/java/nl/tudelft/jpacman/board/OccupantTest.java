89be31d6469e0d5b1fd17abd91909a3c061cadd6
Bigito <makillbig1123@gmail.com>
2023-09-20 23:27:02 +0700

    add detail fucntion

--- src/test/java/nl/tudelft/jpacman/board/OccupantTest.java
+++ src/test/java/nl/tudelft/jpacman/board/OccupantTest.java
@@ -32,7 +32,7 @@ class OccupantTest {
     @Test
     void noStartSquare() {
         // Remove the following placeholder:
-        assertThat(unit).isNotNull();
+        assertThat(unit.hasSquare()).isFalse();
     }
 
     /**
@@ -42,7 +42,9 @@ class OccupantTest {
     @Test
     void testOccupy() {
         // Remove the following placeholder:
-        assertThat(unit).isNotNull();
+        Square square = new BasicSquare();
+        unit.occupy(square);
+        assertThat(unit.hasSquare()).isTrue();
     }
 
     /**
@@ -52,6 +54,11 @@ class OccupantTest {
     @Test
     void testReoccupy() {
         // Remove the following placeholder:
-        assertThat(unit).isNotNull();
+        Square square = new BasicSquare();
+        Square square1 = new BasicSquare();
+        unit.occupy(square);
+        unit.occupy(square1);
+        assertThat(unit.hasSquare()).isTrue();
+        assertThat(unit.getSquare()).isEqualTo(square1);
     }
 }
